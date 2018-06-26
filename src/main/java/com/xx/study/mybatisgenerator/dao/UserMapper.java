57081fb4f1e5da0304daa399991b7e337b26e758
LiaoYuanDou <1013624847@qq.com>
2018-06-26 17:50:03 +0800

    first commit

---
+++ src/main/java/com/xx/study/mybatisgenerator/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xx.study.mybatisgenerator.dao;
+
+import com.xx.study.mybatisgenerator.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
